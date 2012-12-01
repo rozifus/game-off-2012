72c5151d5399d8bb41361b21da2bcda30680ebc2
Ryan Noct <roct@Noct.(none)>
2012-12-01 18:58:03 +1100

    finished level three, is hard.

--- go/Levels/Three.js
+++ go/Levels/Three.js
@@ -11,34 +11,60 @@ go.Level = go.Level || {};
 
 go.Level.Three = {
     title: "Level Three",
-    player: { position: { x:0, y:0, z:0 } },
+    player: { position: { x:-1, y:0, z:-1 } },
     background: 0xeeccdd,
     win_url: "",
     blocks: [ 
         // color, down, left 
         
-        [ go.RED, 3,-1],
+        [ go.RED, 2,-2],
         [ go.YELLOW, 0,-2],
+        [ go.BLUE, -1,-3],
+
+        [ go.RED, 3,1],
+        [ go.BLUE, -1,1],
+        [ go.YELLOW, -1,0],
+
+        [ go.WHITE, 0, 0],
+        [ go.WHITE, 1, 0],
 
         [ go.WHITE, -2, -1],
         [ go.WHITE, -2, -2],
 
-        [ go.WHITE, -1, -1],
         [ go.WHITE, -1, -2],
-        [ go.WHITE, -5, -3],
+        [ go.WHITE, -4, -3],
         [ go.WHITE, -4, -4],
 
         [ go.WHITE, -3, -5],
         [ go.WHITE, -2, -1],
         [ go.WHITE, -2, -2],
-        [ go.WHITE, -2, -4],
+        [ go.WHITE, -2, -5],
         
         [ go.WHITE, -1, -4],
         [ go.WHITE, 0, -5],
         [ go.WHITE, 1, -4],
         [ go.WHITE, 2, -3],
         [ go.WHITE, 1, -2],
+        
+        [ go.WHITE, -4, -2],
+        [ go.WHITE, -5, -1],
+        [ go.WHITE, -5, 0],
+        [ go.WHITE, -5, 1],
 
+        [ go.WHITE, 3, -2],
+        [ go.WHITE, 3, -1],
+        [ go.WHITE, 3, 0],
+        [ go.WHITE, 4, 1],
+        [ go.WHITE, 3, 2],
+
+        [ go.WHITE, 2, 3],
+        [ go.WHITE, 1, 2],
+        [ go.WHITE, 0, 2],
+        [ go.WHITE, -1, 2],
+        [ go.WHITE, -2, 2],
+
+        [ go.WHITE, -3, 2],
+        [ go.WHITE, -4, 1],
 
     ],
 };
